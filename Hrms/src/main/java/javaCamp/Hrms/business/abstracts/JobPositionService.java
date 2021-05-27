42a2fc62ef3535e57db27a2a613473c653b772f0
iskerdogan <79252714+iskerdogan@users.noreply.github.com>
2021-05-27 13:51:52 +0300

    H7

--- Hrms/src/main/java/javaCamp/Hrms/business/abstracts/JobPositionService.java
+++ Hrms/src/main/java/javaCamp/Hrms/business/abstracts/JobPositionService.java
@@ -2,8 +2,13 @@ package javaCamp.Hrms.business.abstracts;
 
 import java.util.List;
 
+import javaCamp.Hrms.core.utilities.result.DataResult;
+import javaCamp.Hrms.core.utilities.result.Result;
 import javaCamp.Hrms.entities.concretes.JobPosition;
 
 public interface JobPositionService {
-	List<JobPosition> getAll();
+	DataResult<List<JobPosition>> getAll();
+	Result add(JobPosition jobPosition);
+	boolean isPositionName(JobPosition jobPosition);
+	 
 }
--- Hrms/src/main/java/javaCamp/Hrms/business/concretes/jobPositionManager.java
+++ Hrms/src/main/java/javaCamp/Hrms/business/concretes/jobPositionManager.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javaCamp.Hrms.business.abstracts.JobPositionService;
+import javaCamp.Hrms.core.utilities.result.DataResult;
+import javaCamp.Hrms.core.utilities.result.ErrorResult;
+import javaCamp.Hrms.core.utilities.result.Result;
+import javaCamp.Hrms.core.utilities.result.SuccessDataResult;
+import javaCamp.Hrms.core.utilities.result.SuccessResult;
 import javaCamp.Hrms.dataAccess.abstracts.JobPositionDao;
 import javaCamp.Hrms.entities.concretes.JobPosition;
 
@@ -21,8 +26,33 @@ public class jobPositionManager implements JobPositionService{
 	}
 
 	@Override
-	public List<JobPosition> getAll() {
-		return this.jobPositionDao.findAll();
+	public DataResult<List<JobPosition>> getAll() {
+		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(),"Pozisyonlar listelendi");
 	}
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		boolean result=this.isPositionName(jobPosition);
+		if (result) {
+			this.jobPositionDao.save(jobPosition);
+			return new SuccessResult("Pozisyon eklendi");
+		}else {
+			return new ErrorResult("Aynı isimde Pozisyon eklendi");
+		}
+		
+	}
+	
+	public boolean isPositionName(JobPosition jobPosition) {
+		boolean result = false;
+		for (JobPosition position :this.getAll().getData()) {
+			if (position.getJobPosition().equals(jobPosition.getJobPosition())) {
+				result = false;
+			}else {
+				result = true;
+			}
+		}
+		return result;
+	}
+
 
 }
